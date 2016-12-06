0361db5081c78730b20f23f1bdfef44d49705400
jmelberg <jordan.melberg@gmail.com>
2016-12-05 22:10:57 -0800

    heroku update 6

--- upspot/settings.py
+++ upspot/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'upspot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
